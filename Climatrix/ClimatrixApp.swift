1b371ea1726398636c971d213070442eed051990
Avik-creator <avikm744@gmail.com>
2024-09-07 21:50:40 +0530

    Initial Commit

---
+++ Climatrix/ClimatrixApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClimatrixApp.swift
+//  Climatrix
+//
+//  Created by AVIK MUKHERJEE on 07/09/24.
+//
+
+import SwiftUI
+
+@main
+struct ClimatrixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
